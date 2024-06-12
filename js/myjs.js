f4e2e2ffcabd555ce5dcbaca4f697a7bb1b9b2a4
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-12 13:48:28 +0200

    add: second game link

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,19 @@
-let outPutUtente = document.querySelector('#messaggio')
+let outPutUtente = document.querySelector('#messaggio');
+let linkGioco = document.querySelector('#linkGioco');
 
-let inputUtente = prompt('Scrivi una parola')
 
-let risposta = isParolaPalindroma(inputUtente)
-outPutUtente.append(risposta)
+// chiedo un input all'utente
+let inputUtente = prompt('Scrivi una parola');
+
+// creao la risposta
+let risposta = isParolaPalindroma(inputUtente);
+outPutUtente.append(risposta);
+
+
+
+
+// inizio gioco pari o dispari con click
+linkGioco.addEventListener('click', () =>{
+    let numeroUser = parseInt(prompt('scrivi un numero da 1 a 5 e gioca contro il computer'))
+    
+});
